3558bf948313c116a06946dd4f6eaadc5218a6e1
Quentinflueckiger <quentin.flueckiger@hispeed.ch>
2018-07-13 18:53:40 +0200

    First steps
    -NavMesh
    -Interactable
    -Click to move / interact
    -Imported couple of assets

---
+++ Side_Unity_Project/Assets/Scripts/WorldInteraction.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class WorldInteraction : MonoBehaviour {
+
+    NavMeshAgent playerAgent;
+
+    private void Start()
+    {
+
+        playerAgent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+
+        // Check if Left mouse button is pressed and the mouse is not on an UI element.
+        if (Input.GetMouseButtonDown(0) && !UnityEngine.EventSystems.EventSystem.current.IsPointerOverGameObject())
+        {
+            GetInteraction();
+        }
+    }
+
+    void GetInteraction()
+    {
+
+        Ray interactionRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit interactionInfo;
+
+        // Check for collision from mouse position to infinity.
+        if (Physics.Raycast(interactionRay, out interactionInfo, Mathf.Infinity))
+        {
+
+            GameObject interactedObject = interactionInfo.collider.gameObject;
+
+            // Check if the object found is an interactable.
+            if(interactedObject.tag == "Interactable Object")
+            {
+                // To be changed, this will not be a click to move game.
+                interactedObject.GetComponent<Interactable>().MoveToInteraction(playerAgent);
+            }
+            else
+            {
+
+                playerAgent.stoppingDistance = 0f;
+                // To be changed, this will not be a click to move game.
+                playerAgent.destination = interactionInfo.point;
+            }
+        }
+    }
+}
